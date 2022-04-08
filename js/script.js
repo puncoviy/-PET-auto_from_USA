4b7f233f8521d11e27f3da5574d4e825861300ae
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-04-08 22:04:36 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = new ChiefSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 5000,
+    });
+  });
